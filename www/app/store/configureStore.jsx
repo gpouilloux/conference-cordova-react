24fcb66ce7d16b2eed7c8f9318dfaeb549e53bab
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-02-09 15:02:35 +0100

    [store] redux store with chrome extension

--- www/app/store/configureStore.jsx
+++ www/app/store/configureStore.jsx
@@ -1,12 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import handleTransitions from 'redux-history-transitions'
+import thunkMiddleware from 'redux-thunk'
 
 import rootReducer from '../reducers'
 
 export default function configureStore(history) {
 
-  // Enhancer to be able to change the URL after a Redux action
-  const enhancer = handleTransitions(history)
+  const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      }) : compose
+
+  const enhancer = composeEnhancers(
+      applyMiddleware(thunkMiddleware),
+      handleTransitions(history)
+    )
 
   const store = createStore(
     rootReducer,
