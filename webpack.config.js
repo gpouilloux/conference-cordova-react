ecb020c76ebd81dfa6fe574fcea15be80002b80f
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-02-10 10:00:18 +0100

    [conf] npm project refactored

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'public');
-var APP_DIR = path.resolve(__dirname, 'app');
+var BUILD_DIR = path.resolve(__dirname, 'www/public');
+var APP_DIR = path.resolve(__dirname, 'www/app');
 
 var config = {
   entry: APP_DIR + '/index.jsx',
