72f3ed2b9d1cddc44791c8ec47abcb80eb3f6b83
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-02-12 16:57:26 +0100

    [aboutPhone] Display device and network information using Cordova plugins

--- www/app/components/aboutPhone.jsx
+++ www/app/components/aboutPhone.jsx
@@ -1,25 +1,47 @@
 import React from 'react'
 
+import {Table, TableBody, TableRow, TableRowColumn} from 'material-ui/Table'
+
 import Header from './header.jsx'
 
 /**
- * Component to list the AboutPhone in a conference
+ * Component to display device related information
+ * using Device and Network information Cordova plugins
  * @author Guillaume Pouilloux <gui.pouilloux@gmail.com>
  */
 const AboutPhone = React.createClass({
-
-  propTypes: {
-  },
 
   render() {
     return (
       <div>
         <Header pageTitle="About the phone" />
-        <p>AboutPhone</p>
+          <Table selectable={false}>
+          <TableBody displayRowCheckbox={false}>
+              <TableRow>
+                <TableRowColumn>Platform</TableRowColumn>
+                <TableRowColumn>{window.device.platform}</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Version</TableRowColumn>
+                <TableRowColumn>{window.device.version}</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>UUID</TableRowColumn>
+                <TableRowColumn>{window.device.uuid}</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Model</TableRowColumn>
+                <TableRowColumn>{window.device.model}</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Connection</TableRowColumn>
+                <TableRowColumn>{window.navigator.connection.type}</TableRowColumn>
+              </TableRow>
+            </TableBody>
+          </Table>
       </div>
     )
   }
-
 })
 
 export default AboutPhone
