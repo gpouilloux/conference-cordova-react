38a24a5a05de100cb69994705474270058fc0b1d
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-02-13 14:38:56 +0100

    [aboutPhone] add app and author information

--- www/app/components/aboutPhone.jsx
+++ www/app/components/aboutPhone.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {Table, TableBody, TableRow, TableRowColumn} from 'material-ui/Table'
 
 import Header from './header.jsx'
+import ProjectInfo from '../../../package.json'
 
 /**
  * Component to display device related information
@@ -12,29 +13,48 @@ import Header from './header.jsx'
 const AboutPhone = React.createClass({
 
   render() {
+    const labelColumnStyle = { width: '38%' }
+
+
     return (
       <div>
-        <Header pageTitle="About the phone" />
+        <Header pageTitle='About the phone' />
           <Table selectable={false}>
           <TableBody displayRowCheckbox={false}>
               <TableRow>
-                <TableRowColumn>Platform</TableRowColumn>
+                <TableRowColumn style={labelColumnStyle}>Description</TableRowColumn>
+                <TableRowColumn>{ProjectInfo.description}</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn style={labelColumnStyle}>App Version</TableRowColumn>
+                <TableRowColumn>{ProjectInfo.version}</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn style={labelColumnStyle}>Author</TableRowColumn>
+                <TableRowColumn>{ProjectInfo.author.name}</TableRowColumn>
+              </TableRow>
+              <TableRow onTouchTap={() => window.cordova.InAppBrowser.open(ProjectInfo.author.url)}>
+                <TableRowColumn style={labelColumnStyle}>Author Website</TableRowColumn>
+                <TableRowColumn style={{textDecoration: 'underline'}}>{ProjectInfo.author.url}</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn style={labelColumnStyle}>Platform</TableRowColumn>
                 <TableRowColumn>{window.device.platform}</TableRowColumn>
               </TableRow>
               <TableRow>
-                <TableRowColumn>Version</TableRowColumn>
+                <TableRowColumn style={labelColumnStyle}>Version</TableRowColumn>
                 <TableRowColumn>{window.device.version}</TableRowColumn>
               </TableRow>
               <TableRow>
-                <TableRowColumn>UUID</TableRowColumn>
+                <TableRowColumn style={labelColumnStyle}>UUID</TableRowColumn>
                 <TableRowColumn>{window.device.uuid}</TableRowColumn>
               </TableRow>
               <TableRow>
-                <TableRowColumn>Model</TableRowColumn>
+                <TableRowColumn style={labelColumnStyle}>Model</TableRowColumn>
                 <TableRowColumn>{window.device.model}</TableRowColumn>
               </TableRow>
               <TableRow>
-                <TableRowColumn>Connection</TableRowColumn>
+                <TableRowColumn style={labelColumnStyle}>Connection</TableRowColumn>
                 <TableRowColumn>{window.navigator.connection.type}</TableRowColumn>
               </TableRow>
             </TableBody>
